0f8228d3d8d552e223446e5105580f7c49bbc263
henok Gebremedhn <henoklwi@gmail.com>
2020-02-07 12:16:21 -0800

    render it to show page of title

--- server.js
+++ server.js
@@ -23,12 +23,14 @@ app.post('/searches', bookHandler);
 function bookHandler(req, res) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
   if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]};` }
-  if (req.body.search[1] === 'author') { url += `+intitle:${req.body.search[0]};` }
-
+  if (req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]};` }
+  console.log(url);
   superagent.get(url)
     .then(data => JSON.parse(data.text).items.map(obj => new Book(obj)))
-    .then(results => res.render('pages/show.ejs', { searchResults: results }))
+    .then(results => res.render('pages/searches/show', { searchResults: results }))
+
     .catch(() => errorHandler('Error 500! Something has gone wrong with the website server!', req, res));
+
 }
 
 
