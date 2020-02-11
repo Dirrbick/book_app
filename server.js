d9d2f417bf9ff32279581b4e1d024f8151f0d3ad
henok Gebremedhn <henoklwi@gmail.com>
2020-02-11 15:02:58 -0800

    start working on the database

--- server.js
+++ server.js
@@ -5,33 +5,75 @@ const express = require('express');
 const PORT = process.env.PORT;
 const app = express();
 const pg = require('pg');
+// const methodOverride = require('_method_Override');
 const client = new pg.Client(process.env.DATABASE_URL);
-
 //set up view engine and serve static CSS files
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride(_method));
+
 app.set('view engine', 'ejs');
 
 app.get('/', homeRender);
 app.get('/searches/new', formRender);
 
+app.post('/books', databaseHandler);
 
 // app.get('/search', searchRender);
-
-
 app.post('/searches', bookHandler);
+
+
+// THIS IS HOME PAGE OF DATABASE SHOW. 
+
+function homeRender(req, res) {
+  // i want to get all the information from the database. 
+  // render it on the index page. 
+  let SQL2 = 'SELECT * FROM books;';
+  client.query(SQL2)
+    .then(results => {
+      console.log('results form database show', results.rows);
+      res.render('pages/index', { databaseResults: results.rows })
+    })
+    .catch(() => errorHandler('error 500! something has gone wrong on the database homeRender', req, res));
+}
+
+
+
+
+//ADDING TO DATABASE . 
+
+function databaseHandler(req, res) {
+  let author = req.body.book_authors;
+  let image_url = req.body.book_image;
+  let title = req.body.book_title;
+  let description = req.body.book_description;
+
+  let SQL1 = 'INSERT INTO books (author , image_url , title , description) VALUES ( $1 , $2 , $3 , $4);';
+  let safeValue = [author, image_url, title, description];
+  client.query(SQL1, safeValue)
+    .then(results => {
+      console.log('this is inside client query');
+    })
+    .catch(() => errorHandler('Error 500 ! something has gone wrong with the database handler!', req, res));
+
+
+}
 
 
 // BOOK HANDLER FUNCTION . 
 function bookHandler(req, res) {
-  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
   if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]};` }
-  if (req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]};` }
+  else { url += `+inauthor:${req.body.search[0]};` }
   superagent.get(url)
-    .then(data => JSON.parse(data.text).items.map(obj => new Book(obj)))
-    .then(results => res.render('pages/searches/show', { searchResults: results }))
+    .then(data => {
+      let results = data.body.items.map(obj => new Book(obj));
+      res.render('pages/searches/show', { searchResults: results })
+    })
 
-    .catch(() => errorHandler('Error 500! Something has gone wrong with the website server!', req, res));
+
+
+    .catch(() => errorHandler('Error 500 ! Something has gone wrong with the  bookHandler !', req, res));
 
 }
 
@@ -43,33 +85,6 @@ function errorHandler(error, req, response) {
 }
 
 
-
-//page renders
-function homeRender(req, res) {
-  let SQL = 'SELECT * FROM books';
-  client.query(SQL)
-    .then(() => {
-      console.log('Db')
-      const books = booksTest;
-      let { author, title, image_url, description } = books;
-      let booksDb = `INSERT INTO books (author, title, image, description) VALUES ('${author}' , '${title}' , '${image_url}' , '${description}')`;
-      client.query(booksDb);
-      console.log(books)
-      res.status(200).send(books);
-
-
-    })
-    .catch(() => errorHandler('Error 500! Something has gone wrong with the website server!', req, res));
-
-
-}
-//test sql 
-var booksTest = {
-  title: 'Harry Potter',
-  author: 'JK Rowling',
-  image: `<img src="http://books.google.com/books/content?id=f280CwAAQBAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"`,
-  description: 'this is the description'
-};
 
 //Form renders .
 function formRender(req, res) {
